55554b3b0f73d1dd41e6abf36ed38883fb35e1fd
Dagna Schmidt <dagdagi889@gmail.com>
2022-12-21 11:59:29 +0100

    about hidden content and styling

--- src/components/About.js
+++ src/components/About.js
@@ -16,6 +16,24 @@ export default function About() {
                 <button className="about__exit btn-round btn-exit" onClick={close}>
                     <i className="fa-solid fa-xmark"></i>
                 </button>
+                <button className="about__more btn-round">
+
+                </button>
+                <div className="about__container">
+                    <div className="about__short">
+                        <p>Junior Web Developer located in Goteborg, Sweden.</p>
+                        <p>Experienced in Interior Design, 
+                        Jewelry Design and as a Dental Technician. 
+                        With passion for Analog Photography.</p>
+                        <p>Professional athlete, multimedalist 
+                        of Polish Championships, former member 
+                        of Polish Powerlifting National Team.</p>
+                    </div>
+                    <img className="about__img" src={require(`../images/portret.png`)} />
+                </div>
+                <p className="about__long">
+
+                </p>
             </div>
         </section>
     )
