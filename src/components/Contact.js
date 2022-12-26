6ae8aa9cd66e1e7585f020ded3bff6787549a9aa
Dagna Schmidt <dagdagi889@gmail.com>
2022-12-27 00:42:36 +0100

    contact content styling and part of form functions added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,6 +9,21 @@ export default function Contact() {
     function close() {
         if(show === true) {setShow(!show)}
     }
+    const [formData, setFormData] = React.useState(
+        {
+            email: "",
+            message: ""
+        }
+    )
+    function handleChange(event) {
+        const {name, value} = event.target
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [name]: value
+            }
+        })
+    }
     return (
         <section className={`contact ${show && "active"}`} onClick={open}>
             <h2 className={`contact__title ${show && "active-title"}`}>Contact</h2>
@@ -19,11 +34,32 @@ export default function Contact() {
                 <div className="contact__container">
                     <div className="contact__card">
                         <form className="contact__card__form">
-                            <label className="contact__card__label"></label>
-                            <input className="contact__card__input" />
-                            <label className="contact__card__label"></label>
-                            <input className="contact__card__input" />
+                            <label className="contact__card__label" htmlFor="name">Your email</label>
+                            <input 
+                                className="contact__card__input" 
+                                placeholder="your@email.com"
+                                type="email" 
+                                onChange={handleChange}
+                                name="name"
+                                id="name"
+                                value={formData.name} 
+                            />
+                            <label className="contact__card__label" htmlFor="message">Your message</label>
+                            <input 
+                                className="contact__card__input" 
+                                placeholder="message"
+                                type="text"
+                                onChange={handleChange}
+                                name="message"
+                                id="message"
+                                value={formData.message} 
+                            />
+                            <button className="contact__card__btn btn-round">
+                                <i className="fa-solid fa-paper-plane"></i>
+                            </button>
+                            <p className="contact__card__send">send</p>
                         </form>
+                        <div className="contact__card__message"></div>
                     </div>
                 </div>
             </div>
