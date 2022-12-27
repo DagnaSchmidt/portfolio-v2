fe46722fbc110b8c407c1b2ea52925101eea7431
Dagna Schmidt <dagdagi889@gmail.com>
2022-12-27 01:06:30 +0100

    function display sended message added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -24,6 +24,13 @@ export default function Contact() {
             }
         })
     }
+    function handleSubmit(event) {
+        event.preventDefault()
+    }
+    const [message, setMessage] = React.useState(false)
+    function showMessage() {
+        setMessage(true)
+    }
     return (
         <section className={`contact ${show && "active"}`} onClick={open}>
             <h2 className={`contact__title ${show && "active-title"}`}>Contact</h2>
@@ -33,15 +40,15 @@ export default function Contact() {
                 </button>
                 <div className="contact__container">
                     <div className="contact__card">
-                        <form className="contact__card__form">
-                            <label className="contact__card__label" htmlFor="name">Your email</label>
+                        <form className="contact__card__form" onSubmit={handleSubmit}>
+                            <label className="contact__card__label" htmlFor="email">Your email</label>
                             <input 
                                 className="contact__card__input" 
                                 placeholder="your@email.com"
                                 type="email" 
                                 onChange={handleChange}
-                                name="name"
-                                id="name"
+                                name="email"
+                                id="email"
                                 value={formData.name} 
                             />
                             <label className="contact__card__label" htmlFor="message">Your message</label>
@@ -54,12 +61,18 @@ export default function Contact() {
                                 id="message"
                                 value={formData.message} 
                             />
-                            <button className="contact__card__btn btn-round">
+                            <button className="contact__card__btn btn-round" onClick={showMessage}>
                                 <i className="fa-solid fa-paper-plane"></i>
                             </button>
                             <p className="contact__card__send">send</p>
                         </form>
-                        <div className="contact__card__message"></div>
+                            {message &&
+                                <div className="contact__card__message">
+                                    <p>Your message has been send!</p>
+                                    <p>Email: <span className="span">{formData.email}</span></p>
+                                    <p>Message: <span className="span">{formData.message}</span></p>
+                                </div>  
+                            }
                     </div>
                 </div>
             </div>
